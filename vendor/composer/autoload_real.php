3a785107e57e3c2f92c6951e9b3362f276f805ca
DevUser40645 <test40645@gmail.com>
2020-08-13 20:51:23 +0300

    5 commit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit947b58a7619bee33ddf06065356299be
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
